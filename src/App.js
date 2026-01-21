085574bba4d2fb5c25218f6c1cc0db866202d7b8
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-01-21 12:10:20 +0000

    adding new delete colaborator feature

--- src/App.js
+++ src/App.js
@@ -219,8 +219,8 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function deletarColaborador() {
-    console.log('Deletar Colaborador')
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id))
   }
 
   function mudarCorDoTime(cor, id) {
